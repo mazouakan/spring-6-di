fea6f87df25a9a980d686e3c5c278147c850a6cb
Mohamed <azouakanmed@gmail.com>
2023-09-24 20:26:36 +0200

    adding profiles example

---
+++ src/test/java/com/azouakan/spring6di/services/i18n/SpanishGreetingServiceTest.java
@@ -0,0 +1,21 @@
+package com.azouakan.spring6di.services.i18n;
+
+import com.azouakan.spring6di.controllers.i18n.Myi18NController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+
+@ActiveProfiles("ES")
+@SpringBootTest
+class SpanishGreetingServiceTest {
+
+    @Autowired
+    Myi18NController controller;
+    @Test
+    void sayGreeting() {
+        System.out.println(controller.sayHello());
+    }
+}
