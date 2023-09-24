c6a76a93957b277094be9221f9f6df424a05d79c
Mohamed <azouakanmed@gmail.com>
2023-09-24 19:31:02 +0200

    adding dep with Spring example

--- src/test/java/com/azouakan/spring6di/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/com/azouakan/spring6di/controllers/ConstructorInjectedControllerTest.java
@@ -1,18 +1,15 @@
 package com.azouakan.spring6di.controllers;
 
-import com.azouakan.spring6di.services.GreetingServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-
-
+@SpringBootTest
 class ConstructorInjectedControllerTest {
 
+    @Autowired
     ConstructorInjectedController controller;
-    @BeforeEach
-    void setUp() {
-        controller = new ConstructorInjectedController(new GreetingServiceImpl());
-    }
 
     @Test
     void sayHello() {
--- src/test/java/com/azouakan/spring6di/controllers/SetterInjectedControllerTest.java
+++ src/test/java/com/azouakan/spring6di/controllers/SetterInjectedControllerTest.java
@@ -1,18 +1,16 @@
 package com.azouakan.spring6di.controllers;
 
-import com.azouakan.spring6di.services.GreetingServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-
+@SpringBootTest
 class SetterInjectedControllerTest {
 
+    @Autowired
     SetterInjectedController controller;
-    @BeforeEach
-    void setUp() {
-        controller = new SetterInjectedController();
-        controller.setGreetingService(new GreetingServiceImpl());
-    }
+
 
     @Test
     void sayHello() {
