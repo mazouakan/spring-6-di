68228d2067beb2b49bc35a10334c735f818eb332
Mohamed <azouakanmed@gmail.com>
2023-09-24 18:18:15 +0200

    adding dep example

--- src/main/java/com/azouakan/spring6di/controllers/MyController.java
+++ src/main/java/com/azouakan/spring6di/controllers/MyController.java
@@ -1,12 +1,21 @@
 package com.azouakan.spring6di.controllers;
 
+import com.azouakan.spring6di.services.GreetingService;
+import com.azouakan.spring6di.services.GreetingServiceImpl;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController() {
+        this.greetingService = new GreetingServiceImpl();
+    }
+
+
     public String sayHello(){
         System.out.println(" I am in MyController");
-        return "Hello Everyone!!!";
+        return greetingService.sayGreeting();
     }
 }
---
+++ src/main/java/com/azouakan/spring6di/services/GreetingServiceImpl.java
@@ -0,0 +1,9 @@
+package com.azouakan.spring6di.services;
+
+public class GreetingServiceImpl implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello Everyone From Base Service!!!";
+    }
+}
